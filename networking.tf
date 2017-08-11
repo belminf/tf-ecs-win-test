f3ec1223474dca26b153eb1d966720aaa75ba5ce
Belmin <me@iambelmin.com>
2017-08-11 01:29:30 +0000

    VPC creation, good

---
+++ networking.tf
@@ -0,0 +1,47 @@
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name    = "ecs-win-test"
+    Project = "ecs-win-test"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags = {
+    Name    = "ecs-win-test"
+    Project = "ecs-win-test"
+  }
+}
+
+resource "aws_route" "default" {
+  route_table_id         = "${aws_vpc.default.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.az1}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "ecs-win-test-sn1"
+    Project = "ecs-win-test"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.az2}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "ecs-win-test-sn2"
+    Project = "ecs-win-test"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,44 @@
+variable "desired_capacity" {
+  type        = "string"
+  description = "ASG desired capacity"
+}
+
+variable "max_size" {
+  type        = "string"
+  description = "ASG max size"
+}
+
+variable "key_name" {
+  type        = "string"
+  description = "Key pair name"
+}
+
+variable "region" {
+  type        = "string"
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "az1" {
+  type        = "string"
+  default     = "us-east-1a"
+  description = "AZ for subnet 1"
+}
+
+variable "az2" {
+  type        = "string"
+  default     = "us-east-1b"
+  description = "AZ for subnet 2"
+}
+
+variable "instance_type" {
+  type        = "string"
+  description = "AWS instance type"
+}
+
+# us-east1 ami: Windows_Server-2016-English-Full-Containers-2017.07.13
+variable "ami" {
+  type        = "string"
+  default     = "ami-25ece233"
+  description = "AMI for Windows"
+}
