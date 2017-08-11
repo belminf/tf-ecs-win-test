d3a5941e71458b41a7fb6dda633aee1d4ef30972
Belmin <me@iambelmin.com>
2017-08-11 03:04:48 +0000

    added listener

--- alb.tf
+++ alb.tf
@@ -1,4 +1,4 @@
-resource "aws_alb" "alb" {
+resource "aws_alb" "default" {
   name            = "ecs-win-test-alb"
   internal        = false
   security_groups = ["${aws_security_group.default.id}"]
@@ -10,13 +10,13 @@ resource "aws_alb" "alb" {
   }
 }
 
-resource "aws_alb_target_group" "tg" {
+resource "aws_alb_target_group" "default" {
   name     = "ecs-win-test-tg"
   vpc_id   = "${aws_vpc.default.id}"
   port     = 80
   protocol = "HTTP"
 
-  health_check = {
+  health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     interval            = 10
@@ -27,5 +27,31 @@ resource "aws_alb_target_group" "tg" {
 
   tags = {
     Project = "ecs-win-test"
+  }
+}
+
+resource "aws_alb_listener" "default" {
+  load_balancer_arn = "${aws_alb.default.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.default.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_alb_listener_rule" "default" {
+  listener_arn = "${aws_alb_listener.default.arn}"
+  priority     = 1
+
+  action {
+    target_group_arn = "${aws_alb_target_group.default.arn}"
+    type             = "forward"
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/"]
   }
 }
